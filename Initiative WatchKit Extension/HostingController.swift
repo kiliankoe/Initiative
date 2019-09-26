4eaa25b8475e16ecf0fe9f794f2bf76ca453bbe9
Kilian Koeltzsch <me@kilian.io>
2019-09-26 15:40:12 +0200

    Initial Commit

---
+++ Initiative WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Initiative WatchKit Extension
+//
+//  Created by Kilian Koeltzsch on 26.09.19.
+//  Copyright © 2019 Kilian Koeltzsch. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
