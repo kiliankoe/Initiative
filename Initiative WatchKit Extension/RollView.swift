857edbb44abfb789c06e6ac27e8cd5ff90a9bbfe
Kilian Koeltzsch <me@kilian.io>
2019-09-26 15:41:15 +0200

    Rename ContentView to RollView

---
+++ Initiative WatchKit Extension/RollView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RollView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct RollView_Previews: PreviewProvider {
+    static var previews: some View {
+        RollView()
+    }
+}
