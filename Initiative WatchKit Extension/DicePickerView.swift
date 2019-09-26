1b0ca68e30231e8d392de0d8e4019f60abbef6ba
Kilian Koeltzsch <me@kilian.io>
2019-09-26 18:12:23 +0200

    Basic UI layout

---
+++ Initiative WatchKit Extension/DicePickerView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct DicePickerView: View {
+    @State var diceCount = 1
+    @State var eyeCount = 20
+
+    var body: some View {
+        HStack {
+            Picker(selection: $diceCount, label: Text("")) {
+                ForEach(1..<21) { val in
+                    Text(String(val)).tag(val)
+                }
+            }
+            Text("d")
+                .font(.system(.title, design: .rounded))
+            Picker(selection: $eyeCount, label: Text("")) {
+                Text("1").tag(1)
+                Text("4").tag(4)
+                Text("6").tag(6)
+                Text("8").tag(8)
+                Text("10").tag(10)
+                Text("12").tag(12)
+                Text("20").tag(20)
+                Text("100").tag(100)
+            }
+        }
+
+//        HStack {
+//            Button("1d20", action: {})
+//            Button("2d6", action: {})
+//            Button("3d8", action: {})
+//        }
+    }
+}
+
+struct DicePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DicePickerView()
+    }
+}
--- Initiative WatchKit Extension/RollView.swift
+++ Initiative WatchKit Extension/RollView.swift
@@ -1,8 +1,30 @@
 import SwiftUI
 
 struct RollView: View {
+    @State var result = "Roll!"
+
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                Image("d20")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Roll Initiative")
+                    .font(.system(.caption, design: .rounded))
+                Spacer()
+            }
+
+            DicePickerView()
+
+            Button(action: {
+                self.result = "20"
+            }, label: {
+                HStack {
+                    Image(systemName: "arrow.2.circlepath")
+                    Text(result)
+                }
+            })
+        }
     }
 }
 
