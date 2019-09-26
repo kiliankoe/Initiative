d2a79c8de7d5a20dda2d49bb029f3d4df6ebbb29
Kilian Koeltzsch <me@kilian.io>
2019-09-26 19:35:43 +0200

    Remove DicePickerView for crit detection
    First SwiftUI project 🙈

--- Initiative WatchKit Extension/RollView.swift
+++ Initiative WatchKit Extension/RollView.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 struct RollView: View {
+    @State var diceCount = 0
+    @State var eyeCount = 20
+
+    @State var isCrit = false
     @State var result = "Roll!"
 
     var body: some View {
@@ -14,24 +18,40 @@ struct RollView: View {
                 Spacer()
             }
 
-            DicePickerView()
+            HStack {
+                Picker("", selection: $diceCount) {
+                    ForEach(1..<21) { val in
+                        Text(String(val)).tag(val)
+                    }
+                }
+                Text("d")
+                    .font(.system(.title, design: .rounded))
+                Picker("", selection: $eyeCount) {
+                    Text("1").tag(1)
+                    Text("4").tag(4)
+                    Text("6").tag(6)
+                    Text("8").tag(8)
+                    Text("10").tag(10)
+                    Text("12").tag(12)
+                    Text("20").tag(20)
+                    Text("100").tag(100)
+                }
+            }
 
             Button(action: {
-                // FIXME
-                let diceCount = 1
-                let diceEyes = 20
-
                 var rollResult = 0
-                for _ in (1...diceCount) {
-                    rollResult += Int.random(in: 1...diceEyes)
+                for _ in (0..<self.diceCount) {
+                    rollResult += Int.random(in: 1...self.eyeCount)
                 }
 
                 self.result = String(rollResult)
 
-                let max = diceCount * diceEyes
+                let max = self.diceCount * self.eyeCount
                 if rollResult == max {
-                    self.result += " 💥"
+                    self.isCrit = true
                     WKInterfaceDevice.current().play(.success)
+                } else {
+                    self.isCrit = false
                 }
             }, label: {
                 HStack {
@@ -39,6 +59,10 @@ struct RollView: View {
                     Text(result)
                 }
             })
+                .font(.system(.headline, design: .rounded))
+                .background(isCrit ? Color.green : Color.black)
+                .foregroundColor(isCrit ? .black : .white)
+                .cornerRadius(20)
         }
     }
 }
