f5e13a4df54889af1326ece0b7ff8ec4b20fc675
Kilian Koeltzsch <me@kilian.io>
2020-06-26 21:48:06 +0200

    Better tracking of roll result
    also show critical failures

--- Initiative WatchKit Extension/RollButton.swift
+++ Initiative WatchKit Extension/RollButton.swift
@@ -1,46 +1,110 @@
 import SwiftUI
 
+enum RollResult {
+    case unrolled
+    case roll(Int)
+}
+
+enum RollType {
+    case criticalFailure
+    case normal
+    case criticalSuccess
+}
+
 struct RollButton: View {
     @Binding var selectedDiceCount: Int
     @Binding var selectedDie: Int
 
-    @State var isCrit = false
-    @State var result = "Roll!"
+    @State var result: RollResult = .unrolled
+    @State var type: RollType = .normal
+
+    var stateIcon: Image {
+        switch type {
+        case .normal:
+            return Image(systemName: "arrow.triangle.2.circlepath")
+        case .criticalFailure:
+            return Image(systemName: "xmark.circle")
+        case .criticalSuccess:
+            return Image(systemName: "checkmark.circle")
+        }
+    }
+
+    var backgroundColor: Color {
+        switch type {
+        case .normal:
+            return .black
+        case .criticalSuccess:
+            return .green
+        case .criticalFailure:
+            return .red
+        }
+    }
+
+    var foregroundColor: Color {
+        switch type {
+        case .normal:
+            return .white
+        default:
+            return .black
+        }
+    }
 
     var body: some View {
-        Button(action: {
+        Button {
             var rollResult = 0
-            for _ in (1...self.selectedDiceCount) {
-                rollResult += Int.random(in: 1...self.selectedDie)
+            for _ in (1...selectedDiceCount) {
+                rollResult += Int.random(in: 1...selectedDie)
             }
+            self.result = .roll(rollResult)
 
-            self.result = String(rollResult)
+            let max = selectedDiceCount * selectedDie
+            let min = selectedDiceCount
 
-            let max = self.selectedDiceCount * self.selectedDie
             if rollResult == max {
-                self.isCrit = true
+                type = .criticalSuccess
                 WKInterfaceDevice.current().play(.success)
+            } else if rollResult == min {
+                type = .criticalFailure
+                WKInterfaceDevice.current().play(.failure)
             } else {
-                self.isCrit = false
+                type = .normal
             }
-        }, label: {
-            HStack {
-                Image(systemName: "arrow.2.circlepath")
-                Text(result)
+        } label: {
+            switch result {
+            case .unrolled:
+                Text("Roll!")
+            case .roll(let roll):
+                HStack {
+                    stateIcon
+                    Text(String(roll))
+                }
             }
-        })
-            .font(.system(.headline, design: .rounded))
-            .background(isCrit ? Color.green : Color.black)
-            .foregroundColor(isCrit ? .black : .white)
-            .cornerRadius(20)
+        }
+        .font(.system(.headline, design: .rounded))
+        .background(backgroundColor)
+        .foregroundColor(foregroundColor)
+        .cornerRadius(20)
     }
 }
 
 struct RollButton_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            RollButton(selectedDiceCount: .constant(1), selectedDie: .constant(20))
-            RollButton(selectedDiceCount: .constant(1), selectedDie: .constant(20), isCrit: true, result: "20")
+            RollButton(
+                selectedDiceCount: .constant(1),
+                selectedDie: .constant(20))
+            RollButton(
+                selectedDiceCount: .constant(1),
+                selectedDie: .constant(20),
+                result: .roll(1),
+                type: .criticalFailure)
+            RollButton(
+                selectedDiceCount: .constant(1),
+                selectedDie: .constant(20),
+                result: .roll(20),
+                type: .criticalSuccess)
         }
+        .padding()
+//        .previewLayout(.sizeThatFits)
     }
 }
